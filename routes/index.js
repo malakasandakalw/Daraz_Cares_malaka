cbe08082806b8780933d3936b1901467cac18fbe
Your Name <you@example.com>
2021-08-23 14:04:42 +0530

    Homepage_updates

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Daraz Cares' });
 });
 
-router.get('life', function(req, res, next) {
+router.get('/life', function(req, res, next) {
   res.render('life', { title: 'Life at Daraz' });
 });
 
