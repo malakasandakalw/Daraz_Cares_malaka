1d249ad4fea25ab02829ac42468d0a453aa54aff
Your Name <you@example.com>
2021-08-23 13:14:28 +0530

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Daraz Cares' });
+});
+
+router.get('life', function(req, res, next) {
+  res.render('life', { title: 'Life at Daraz' });
+});
+
+module.exports = router;
