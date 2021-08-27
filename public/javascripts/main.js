0a6bd2157ebeda5f2b86670f37c2b71f5d117c2c
Your Name <you@example.com>
2021-08-27 13:03:39 +0530

    updates

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,17 +1,17 @@
-$(document).ready(function(){
-    /* Get iframe src attribute value i.e. YouTube video url
-    and store it in a variable */
-    var url = $("#mainvideo").attr('src');
+// $(document).ready(function(){
+//     /* Get iframe src attribute value i.e. YouTube video url
+//     and store it in a variable */
+//     var url = $("#mainvideo").attr('src');
     
-    /* Assign empty url value to the iframe src attribute when
-    modal hide, which stop the video playing */
-    $("#myModal").on('hide.bs.modal', function(){
-        $("#mainvideo").attr('src', '');
-    });
+//     /* Assign empty url value to the iframe src attribute when
+//     modal hide, which stop the video playing */
+//     $("#myModal").on('hide.bs.modal', function(){
+//         $("#mainvideo").attr('src', '');
+//     });
     
-    /* Assign the initially stored url back to the iframe src
-    attribute when modal is displayed again */
-    $("#myModal").on('show.bs.modal', function(){
-        $("#mainvideo").attr('src', url);
-    });
-});
+//     /* Assign the initially stored url back to the iframe src
+//     attribute when modal is displayed again */
+//     $("#myModal").on('show.bs.modal', function(){
+//         $("#mainvideo").attr('src', url);
+//     });
+// });
