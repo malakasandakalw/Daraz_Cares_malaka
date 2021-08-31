81e5ddc45b77957c236efa0126ccc680ab0b76d1
Your Name <you@example.com>
2021-08-31 09:34:50 +0530

    updates

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,31 +1,16 @@
-// $(document).ready(function(){
-//     /* Get iframe src attribute value i.e. YouTube video url
-//     and store it in a variable */
-//     var url = $("#mainvideo").attr('src');
-    
-//     /* Assign empty url value to the iframe src attribute when
-//     modal hide, which stop the video playing */
-//     $("#myModal").on('hide.bs.modal', function(){
-//         $("#mainvideo").attr('src', '');
-//     });
-    
-//     /* Assign the initially stored url back to the iframe src
-//     attribute when modal is displayed again */
-//     $("#myModal").on('show.bs.modal', function(){
-//         $("#mainvideo").attr('src', url);
-//     });
-// });
-var acc = document.getElementsByClassName("accordion");
-var i;
-
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
+$(document).ready(function(){
+  $(".accordion-btn-1").click(function(){
+    var chk = $('#year2018').hasClass('move-top')
+    if(!chk){
+        $('#year2018').addClass('move-top');
     } else {
-      panel.style.display = "block";
+        $('#year2018').removeClass('move-top');
+    }
+    var chk2 = $('#list-item').hasClass('border-on')
+    if(!chk2){
+        $('#list-item').addClass('border-on');
+    }else{
+        $('#list-item').removeClass('border-on');
     }
   });
-}
+});
